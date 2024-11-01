349848c85ea06ca00ac35bade97d342d0b68b5a1
murilo-tonassi <murilo.tonassi@alunoescolassociais.org.br>
2024-11-01 07:46:26 -0300

    implementa função para buscar taxa de câmbio via API

--- conversor.js
+++ conversor.js
@@ -2,3 +2,21 @@ const apiKey = `e97064e0a15ceefc5ce3d309`;
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
 
 
+// Função para buscar taxa de câmbio via API
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+async function geExchangeRate(daMoeda, paraMoeda){
+    try{
+        const response = await fetch(`${apiURL}${daMoeda}`);
+        const data = await response.json();
+
+        if(data.result === "sucess"){
+            return data.conversation_rates[paraMoeda];
+        }else{
+            throw new Error('Erro ao buscar as taxas de câmbio');
+        }
+    }catch{
+
+    }
+}
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
